db3b41f9d10a918d5fd9ce0054a9d41a39bf5036
planethouki <26595148+planethouki@users.noreply.github.com>
2018-12-24 23:06:57 +0900

    inboxmarkup to ld+json

--- extractUrl/index.js
+++ extractUrl/index.js
@@ -4,15 +4,14 @@ module.exports = async function (context, req) {
     if (req.body) {
         
         const mailText = req.body;
-        const searchTerm = '<script type="application/json" data-scope="inboxmarkup">';
+        const searchTerm = '<script type="application/ld+json">';
         let output = "";
         if (mailText.includes(searchTerm)) {
             const start = mailText.indexOf(searchTerm);
             const end = mailText.indexOf('</script>', start);
             const json = mailText.substring(start + searchTerm.length, end);
             const obj = JSON.parse(json);
-            output = obj.updates.action.url;
-
+            output = obj[0].potentialAction.url
         }
         context.res = {
             // status: 200, /* Defaults to 200 */
